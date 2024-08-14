b64c8ee9b7a8055040498cd6700a50f8d8ad9b06
Owais Bhatti <owaisidrees87@gmail.com>
2024-08-14 13:29:26 -0600

    Screen divided

--- App.tsx
+++ App.tsx
@@ -1,16 +1,63 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 
 
 const App=()=>{
   return(
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'
-    }}>
-      <Text style={{fontSize:40,color:'red'}}>Hello World</Text>
+    <View style={Style.container}>
+      <View style={Style.upperscreen}>
+        <View style={Style.upperfirst}>
+
+        </View>
+        <View style={Style.uppersecond}>
+          <View style={Style.one}></View>
+          <View style={Style.two}></View>
+
+        </View>
+
+      </View>
+      <View style={Style.lowerscreen}>
+
+      </View>
+
     </View>
   );
 };
 
+const Style=StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  upperscreen:{
+    flex:1,
+    backgroundColor:'red',
+    flexDirection:'row'
+  },
+  lowerscreen:{
+    flex:1,
+    backgroundColor:'blue',
+    margin:5
+  },
+  upperfirst:{
+    flex:1,
+    backgroundColor:'yellow',
+    margin:5
+  },
+  uppersecond:{
+    flex:1,
+    backgroundColor:'grey'
+  },
+  one:{
+    flex:1,
+    backgroundColor:'green',
+    margin:5
+  },
+  two:{
+    flex:1,
+    backgroundColor:'purple',
+    margin:5
+  }
+})
 
 
 export default App;
